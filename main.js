8b325bb8016da1c88f093fd50b8bcc54fefb75d7
Gen Liu <gerald_721@hotmail.com>
2020-07-26 23:03:37 +0800

    update dependencies and add packager

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 
-let win
-
 function createWindow() {
-  win = new BrowserWindow({ width: 640, height: 480 })
-  win.loadFile('index.html')
-  win.on('closed', () => {
-    win = null
+  var win = new BrowserWindow({
+    width: 640,
+    height: 480,
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
+  win.loadFile('index.html')
 }
 
-app.on('ready', createWindow)
+app.whenReady().then(createWindow)
